0d4f72e563ea80c6935ff649be75e785d8987a97
Дмитрий Кулясов <vvvvvvmaz@gmail.com>
2024-05-06 16:21:37 +0300

    refactor even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,25 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
-import readAnswer from '../src/cli.js';
+import readUsername from '../src/cli.js';
 
 function isEven(number) {
   return number % 2 === 0;
 }
 
-function evenGame() {
-  console.log('Welcome to the Brain Games!');
-  const userName = readAnswer();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export default function evenGame() {
   const tryNumbers = 3;
+  const maxRandomNumber = 100;
+  const minRandomNumber = 1;
+  console.log('Welcome to the Brain Games!');
+  const userName = readUsername();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < tryNumbers; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100);
+    const randomNumber = getRandomNumber(minRandomNumber, maxRandomNumber);
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
     const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
