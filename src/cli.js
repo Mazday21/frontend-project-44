9de7c8843312609104661fd3acdbd78de5e60f83
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-13 08:51:54 +0300

    corrections

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-function readAnswer() {
+function makeWelcome() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 }
-export default readAnswer;
+export default makeWelcome;
