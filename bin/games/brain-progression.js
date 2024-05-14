70c645fe68c201e2f9772b03649917e80ba90312
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-14 10:51:48 +0300

    code climate issues

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -6,29 +6,6 @@ import { roundsCount, maxRandomNumber, minRandomNumber } from '../../src/GameCon
 const description = 'What number is missing in the progression?';
 const amountNums = 10;
 
-// export default function progressiveGame() {
-//   const gameData = [];
-//   for (let i = 0; i < roundsCount; i += 1) {
-//     question = [];
-//     const startNumber = getRandomNumber(minRandomNumber, maxRandomNumber);
-//     const differenceNumber = getRandomNumber(minRandomNumber, maxRandomNumber);
-//     const blankPosition = getRandomNumber(minRandomNumber, amountNums);
-//     let correctAnswer;
-//     for (let j = 0; j < amountNums; j += 1) {
-//       const currentNumber = startNumber + j * differenceNumber;
-//       if (j === blankPosition) {
-//         question.push(blank);
-//         correctAnswer = currentNumber;
-//       } else {
-//         question.push(currentNumber.toString());
-//       }
-//     }
-//     gameData.push([question.join(' '), correctAnswer.toString()]);
-//   }
-//   generalLogic(gameData, description);
-// }
-
-// progressiveGame();
 function genProg(startNum, diffNum, amounNums, blankPos) {
   const progression = [];
   let correctAnswer;
@@ -42,7 +19,6 @@ function genProg(startNum, diffNum, amounNums, blankPos) {
       progression.push(currentNumber.toString());
     }
   }
-
   return [progression.join(' '), correctAnswer.toString()];
 }
 
